9676defaa12298cd758e32f9c6996931a27e9b00
Param bhansali <bhansaliparam@gmail.com>
2023-08-27 17:52:58 +0530

    Added Payment Model

--- src/components/contract-creation/index.jsx
+++ src/components/contract-creation/index.jsx
@@ -33,6 +33,7 @@ export function ContractCreation(props){
     const [deadlineRange, setDeadlineRange] = useState("Days");
     const [userInput, setUserInput] = useState("");
     const [viewPrice, setViewPrice] = useState();
+    const [activeModel, setActiveModel] = useState("Agreement");
 
     useEffect(() => {
         if(userType == 2){
@@ -176,9 +177,11 @@ export function ContractCreation(props){
     }
 
     return (<>
-        <Modal isOpen={props.isOpen} closeModal={props.closeModal} big={true}>
+        <Modal isOpen={props.isOpen} closeModal={props.closeModal} big={true} activeModel={activeModel} setActiveModel={setActiveModel}>  
+        <>
+        {activeModel === "Agreement" && (<> 
             {(step == 0) && <SelectUserType nextStep={setStep} step={step} userType={userType} setUserType={switchUserType}/>}
-            {(step == 1) && <UserAddress userInput={userInput} setUserInput={setUserInput} nextStep={setStep} step={step} userType={userType} client={client} serviceProvider={serviceProvider} setClient={setClient} setServiceProvider={setServiceProvider} nextLoading={nextLoading} setNextLoading                         ={setNextLoading                         }/>}
+            {(step == 1) && <UserAddress userInput={userInput} setUserInput={setUserInput} nextStep={setStep} step={step} userType={userType} client={client} serviceProvider={serviceProvider} setClient={setClient} setServiceProvider={setServiceProvider} nextLoading={nextLoading} setNextLoading={setNextLoading}/>}
             {(step == 2) && <Deadline nextStep={setStep} step={step} deadlineValue={deadlineValue} setDeadlineValue={setDeadlineValue} deadlineRange={deadlineRange} setDeadlineRange={setDeadlineRange}/>}
             {(step == 3) && <AgreementDetails nextStep={setStep} step={step} details={details} setDetails={setDetails} />}
             {(step == 4) && <Deliverables nextStep={setStep} step={step} delivery={delivery} setDelivery={setDelivery}  />}
@@ -187,6 +190,17 @@ export function ContractCreation(props){
                                   setPrice={setPrice} userType={userType} tokens={CONTRACT[chainId].tokens} setSelectedToken={setSelectedToken}
                                   selectedToken={selectedToken} setViewPrice={setViewPrice} viewPrice={viewPrice}/>}
             {(step == 6) && <Skills nextLoading={nextLoading} nextStep={setStep} step={step} sign={createAndSignAgreement} skills={skills} setSkills={setSkills}  />}
+        </> ) }
+        {activeModel === "Payment" && (<> 
+            {(step == 0) && <SelectUserType nextStep={setStep} step={step} userType={userType} setUserType={switchUserType}/>}
+            {(step == 1) && <UserAddress userInput={userInput} setUserInput={setUserInput} nextStep={setStep} step={step} userType={userType} client={client} serviceProvider={serviceProvider} setClient={setClient} setServiceProvider={setServiceProvider} nextLoading={nextLoading} setNextLoading={setNextLoading}/>}
+            {(step == 2) && <Price approveTokens={approveTokens} nextLoading={nextLoading} nextStep={setStep} step={step}
+                                 sign={createAndSignAgreement} allowance={allowance} price={price}
+                                  setPrice={setPrice} userType={userType} tokens={CONTRACT[chainId].tokens} setSelectedToken={setSelectedToken}
+                                  selectedToken={selectedToken} setViewPrice={setViewPrice} viewPrice={viewPrice}/>}
+        </>)}
+            
+            </>
         </Modal>
        
         {/* {
--- src/components/modal/index.jsx
+++ src/components/modal/index.jsx
@@ -8,11 +8,20 @@ export function Modal(props){
                 <div className="mask" onClick={() => props.closeModal(false)}></div>
                 <div className={'modal ' + (props.big && "big")}>
                     <div className="modal-head">
-                        <div className="close" onClick={() => props.closeModal(false)}>x</div>
+                        <div className="toggle-btn">
+                            <button id="btn1" className={props.activeModel === "Agreement" ? "active" : ""}
+                                    onClick={() => props.setActiveModel("Agreement")} >Agreement</button>
+                            <button id="btn2" 
+                            // className={props.activeModel === "Payment" ? "active" : ""} 
+                            //         onClick={() => props.setActiveModel("Payment")} ---> For Payment Model
+                                    >Payment</button>
+                        </div>
+                        <div className="close" onClick={() => props.closeModal(false)}>X</div>
                     </div>
                     <div className="modal-content">
                     {props.children}
                     </div>
+                    
                 </div>
             </>
         )}

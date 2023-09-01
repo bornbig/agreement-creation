8ad303775f9d8c44507aa321d95c1d9d95e20155
bornbig <72680443+bornbig@users.noreply.github.com>
2023-09-01 23:43:20 +0530

    Small Ui changes

--- src/components/modal/index.jsx
+++ src/components/modal/index.jsx
@@ -9,12 +9,12 @@ export function Modal(props){
                 <div className={'modal ' + (props.big && "big")}>
                     <div className="modal-head">
                         <div className="toggle-btn">
-                            <button id="btn1" className={props.activeModel === "Agreement" ? "active" : ""}
-                                    onClick={() => props.setActiveModel("Agreement")} >Agreement</button>
-                            <button id="btn2" 
-                            className={props.activeModel === "Payment" ? "active" : ""} 
+                            <div className={"tab " + (props.activeModel === "Agreement" && "active" )}
+                                    onClick={() => props.setActiveModel("Agreement")} >Agreement</div>
+                            <div 
+                            className={"tab " + (props.activeModel === "Payment" && "active")} 
                                     onClick={() => props.setActiveModel("Payment")}
-                                    >Payment</button>
+                                    >Payment</div>
                         </div>
                         <div className="close" onClick={() => props.closeModal(false)}>X</div>
                     </div>
